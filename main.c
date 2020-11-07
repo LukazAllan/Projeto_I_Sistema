24e7b7c95e522efccba8e1f5b84d1d30fe2cfd4a
LukazAllan <allanlucas130@gmail.com>
2020-11-07 18:38:08 -0300

    structurando o codigo

--- main.c
+++ main.c
@@ -2,78 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 
-int gestor() {
-    /*Esta função corresponde ao cargo de gestor*/
-    FILE *fp = fopen("gestor.txt", "r");
-    if (fp == NULL){
-        printf("File not open error\n");
-        return -1;
-    }
+typedef struct obra{
     
-    return 0;
-}
+};
 
-int eng() {
-    /*Esta função corresponde ao cargo de engenheiro*/
-    
-    return 0;
-}
+typedef struct state{
+
+};
 
 int main() {
-    int i, logado = 0, not_email = 1, email_aceito = 0, senha_aceita = 0, key = 0;
+    int i, logado = 0, not_email = 1, email_aceito = 0, senha_aceita = 0, key = 0, on = 1;
     int info_p_linha = 3;
-    char email[24], senha[24];
+    char email[24], senha[24], q = 'z';
     //                             "e-mail", "senha", "função"
     char* login[] = {"allanlucas@empresa.com", "123", "E",
                      "alanamorais@empresa.com", "321", "F",
                      "lirielterto@empresa.com", "012", "M",
                      "lucaslins@iesp.edu.br", "000", "G"
                     };
-    
-    // Login
-    while (logado == 0) {
-        do{
-            printf("Digite seu e-mail: ");
-            scanf("%s", &email);
+    while (on != 0){
+        // Login
+        while (logado == 0) {
+            do{
+                printf("Digite seu e-mail: ");
+                scanf("%s", &email);
 
-            for (i = 0; i < 12; i=i+info_p_linha) {
-                //printf("%s: %i\n", login[i], i);
-                if (strcmp(email, login[i]) == 0) {
-                    email_aceito = 1;
-                    not_email = 0;
-                    key = i;
+                for (i = 0; i < 12; i=i+info_p_linha) {
+                    //printf("%s: %i\n", login[i], i);
+                    if (strcmp(email, login[i]) == 0) {
+                        email_aceito = 1;
+                        not_email = 0;
+                        key = i;
+                    }
+                    if (email_aceito == 1) {
+                        i = 999;
+                    }
                 }
-                if (email_aceito == 1) {
-                    i = 999;
-                }
-            }
-        
-        } while (not_email == 1);
-        
-        i=0;
-        //printf("%i\n", key);
+            
+            } while (not_email == 1);
+            
+            i=0;
+            //printf("%i\n", key);
 
-        do{
-            if (i > 0) {
-                printf("Tente Novamente!\n");
-            }
-            i++;
-            printf("Digite sua senha: ");
-            scanf("%s", &senha);
-            if (strcmp(senha, login[key+1]) == 0) {
-                senha_aceita = 1;
-                logado = 1;
-                printf("Logado!\n");
-            }
-        } while ((senha_aceita == 0) && (i < 5));
-    }
-    
-    if (strcmp(login[key+2], "G") == 0) {
-        gestor();
-    } else if (strcmp(login[key+2], "E") == 0) {
-        eng();
-    } else {
-        printf("ERROR FATAL!!! USER TYPE NOT RECOGNIZED!!!\n");
+            do{
+                if (i > 0) {
+                    printf("Tente Novamente!\n");
+                }
+                i++;
+                printf("Digite sua senha: ");
+                scanf("%s", &senha);
+                if (strcmp(senha, login[key+1]) == 0) {
+                    senha_aceita = 1;
+                    logado = 1;
+                    printf("Logado!\n");
+                }
+            } while ((senha_aceita == 0) && (i < 5));
+        }
+        
+        if (strcmp(login[key+2], "G") == 0) {
+            gestor();
+        } else if (strcmp(login[key+2], "E") == 0) {
+            eng();
+        } else {
+            printf("ERROR FATAL!!! USER TYPE NOT RECOGNIZED!!!\n");
+        }
+
+        printf("DESEJA SAIR [s/n]: ");
+        scanf("%c", &q);
+        switch (q){
+          case 's':
+            on = 0;
+            break;
+
+          default:
+            break;
+        }
     }
     return 0;
 }
